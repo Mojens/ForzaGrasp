b8d2f6ca3823de7d5723f6f971961ee5162383e4
Mojens <moha135h@stud.kea.dk>
2021-11-15 21:00:39 +0100

    Generisk Menu tilføjet

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,44 @@
+import java.util.Scanner;
+
 public class Menu {
+
+  String menuHeader;
+  String leadText;
+  String[] menuItems;
+
+  public Menu(String menuHeader, String leadText, String[] menuItems) {
+    this.menuHeader = menuHeader;
+    this.leadText = leadText;
+    this.menuItems = menuItems;
+  }
+
+  public void printMenu() {
+    System.out.println(menuHeader);
+    for (int i = 0; i < menuItems.length; i++) {
+      if (menuItems[i] != null) {
+        System.out.print(i + ". ");
+        System.out.println(menuItems[i]);
+      }
+    }
+    System.out.print(leadText);
+  }
+
+  public int readChoice() {
+    Scanner input = new Scanner(System.in);
+    while (!input.hasNextInt()) {
+      System.out.println("\nIt has to be between 1,2,3 or 9");
+      System.out.println("Try again");
+      System.out.println(menuHeader);
+
+      for (int i = 0; i < menuItems.length; i++) {
+        if (menuItems[i] != null) {
+          System.out.print(i + ". ");
+          System.out.println(menuItems[i]);
+        }
+      }
+      System.out.print(leadText);
+      input.nextLine();
+    }
+    return input.nextInt();
+  }
 }
--- src/MenuRun.java
+++ src/MenuRun.java
@@ -1,2 +1,46 @@
 public class MenuRun {
+
+  private void run() {
+    String[] menuItem = new String[10];
+
+    menuItem[1] = "Choose ONE";
+    menuItem[2] = "Choose TWO";
+    menuItem[3] = "Choose THREE";
+    menuItem[9] = "QUIT";
+
+    Menu menu = new Menu("MENU", "PLEASE CHOOSE HERE: ", menuItem);
+    menu.printMenu();
+
+    int choice = menu.readChoice();
+    boolean checkChoice;
+
+    do {
+      switch (choice) {
+        case 1:
+          System.out.println("You choosed 1");
+          checkChoice = true;
+          break;
+        case 2:
+          System.out.println("You choosed 2");
+          checkChoice = true;
+          break;
+        case 3:
+          System.out.println("You choosed 3");
+          checkChoice = true;
+          break;
+        case 9:
+          System.out.println("You choosed QUIT");
+          checkChoice = true;
+          break;
+
+        default:
+          System.out.println("\nIt has to be between 1,2,3 or 9");
+          System.out.println("Try again");
+          menu.printMenu();
+          choice = menu.readChoice();
+          checkChoice = false;
+      }
+    } while (!checkChoice);
+  }
 }
+
