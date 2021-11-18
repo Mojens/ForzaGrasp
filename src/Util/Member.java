0e723dbdb0bc16a1b68fb9e8b0f3f9b0b256a660
Mojens <moha135h@stud.kea.dk>
2021-11-18 15:54:25 +0100

    Changes by mholm

--- src/Util/Member.java
+++ src/Util/Member.java
@@ -18,4 +18,22 @@ public class Member{
     private String location;
     private int date;
 
+    public Member(String name, int age, Status status, String ageGroup, String level, double balance ){
+    this.name = name;
+    this.age = age;
+    this.ageGroup = ageGroup;
+    this.level = level;
+    this.balance = balance;
+
+
+    }
+    public Member(String name, int age, Status status, String ageGroup, String level, String style ){
+        this.name = name;
+        this.age = age;
+        this.ageGroup = ageGroup;
+        this.level = level;
+        this.style = style;
+
+
+    }
 }
