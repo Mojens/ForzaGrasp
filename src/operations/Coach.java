30d2fc9f2528893fbc80b6022f50a529f0c500c6
ChineseThunder <simo09j9@stud.kea.dk>
2021-11-20 11:16:34 +0100

    h

--- src/operations/Coach.java
+++ src/operations/Coach.java
@@ -1,6 +1,21 @@
 package operations;
+import util.Member;
+import util.MemberList;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @Author Simon
  */
-public class Coach {
+
+public class Coach extends Member{
+
+    public void memberTime() throws IOException{
+        setToStringStatus(1);
+        Scanner input = new Scanner(System.in);
+        System.out.println("type name");
+
+    }
+
 }
