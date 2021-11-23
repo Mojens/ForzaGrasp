6e4460b9bb4605fb187664aa104510358451f073
ChineseThunder <simo09j9@stud.kea.dk>
2021-11-23 11:43:47 +0100

    if else arraylister på practise

--- src/util/TrainingResults.java
+++ src/util/TrainingResults.java
@@ -7,6 +7,13 @@ import java.util.ArrayList;
  */
 public class TrainingResults {
 
-  public static ArrayList<Member> practiceTime = new ArrayList<>();
+  public static ArrayList <Member> jrButtrfly = new ArrayList<>();
+  public static ArrayList <Member> jrCrawl = new ArrayList<>();
+  public static ArrayList <Member> jrRygcrawl = new ArrayList<>();
+  public static ArrayList <Member> jrBrystsvømning = new ArrayList<>();
 
+  public static ArrayList <Member> seniorButtrfly = new ArrayList<>();
+  public static ArrayList <Member> seniorCrawl = new ArrayList<>();
+  public static ArrayList <Member> seniorRygcrawl = new ArrayList<>();
+  public static ArrayList <Member> seniorBrystsvømning = new ArrayList<>();
 }
